74aca85fb32199ed7c5cd21f5e3b6f368b9f5b87
Aayush <aayushpatel7012006@gmail.com>
2026-06-22 12:12:33 +0530

    new potfolio

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="bg-slate-950 text-white">
+      <Navbar />
+      <Home />
+      <About />
+      <Education />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
